5d976f3eafe7c3ff3333509d865d2d11b17e6eaf
DanB1999 <83217388+DanB1999@users.noreply.github.com>
2023-05-30 21:27:06 +0200

    Update VM.py

--- VM.py
+++ VM.py
@@ -1,24 +1,52 @@
 import sys
+from typing import Union
 import libvirt
 from libvirt import virDomain, libvirtError
 from pydantic import BaseModel
+import xml.dom.minidom
 
+from exceptions import APIError, ConnectionFailed, DomainAlreadyRunning, DomainNotRunning, RessourceNotFound
 
-class VM():
-    def __init__(self):
+class DomainObj(BaseModel):
+    name: Union[str, None] = None
+    memory: int
+    vcpu: int
+    source_file: str
+    emulator: str
+    
+    class Config:
+            schema_extra = {
+                "example": {
+                    "name": "vm1",
+                    "memory": 500000,
+                    "vcpu": 1,
+                    "source_file": "/var/lib/libvirt/isos/debian-11.6.0-amd64-netinst.iso",
+                    "emulator": "/usr/bin/qemu-system-x86_64"
+                }
+            }
+
+class VM():   
+    def libvirtConnect(self):
         try:
-            self.conn=libvirt.open("qemu:///system")
-        except libvirtError as e:
-            print(repr(e), file = sys.stderr)
-            exit(1)
+            conn = libvirt.open('qemu:///system')
+            return conn
+        except libvirt.libvirtError:
+            raise ConnectionFailed()
 
-    def checkConnection(self):
-        return 0
-
+    def getDomainByUUID(self, id):
+        conn = self.libvirtConnect()
+        domains = conn.listAllDomains()
+        for dom in domains:
+            uuid = dom.UUIDString()
+            if uuid == id:
+                return dom
+        raise RessourceNotFound()
+            
     def listDomains(self):
-        domains = {}
+        conn = self.libvirtConnect()
         try:
-            domains = self.conn.listAllDomains()
+            domains = {}
+            domains = conn.listAllDomains()
             res = []
             for dom in domains:
                 uuid = dom.UUIDString()
@@ -26,20 +54,23 @@ class VM():
                                 "name": dom.name(),
                                 #"hostname": domain.hostname(),
                                 "isActive": dom.isActive(),
+                                "status": self.getDomStatus(dom).get("desc"),
                                 "isPersistent": dom.isPersistent()
                                 })
             return res
         except libvirtError as e:
             res = repr(e)
-        return res
+            return res
 
     def getDomainStats(self, id):
+        conn = self.libvirtConnect()
         dom = self.getDomainByUUID(id)
         state, maxmem, mem, cpus, cput = dom.info()
         return {"uuid": dom.UUIDString(),
                 "name": dom.name(),
                 #"hostname": dom.hostname(),
                 "isActive": dom.isActive(),
+                "status": self.getDomStatus(dom).get("desc"),
                 "isPersistent": dom.isPersistent(),
                 "OSType": dom.OSType(),
                 "hasCurrentSnapshot": dom.hasCurrentSnapshot(),
@@ -53,30 +84,86 @@ class VM():
                 }
     
 
+#VM nach start immernoch pausiert (im virt-manager)
 
     def startVM(self, id):
-        domain = self.getDomainByUUID(id)
+        conn = self.libvirtConnect()
+        dom = self.getDomainByUUID(id)
         #stream=self.conn.newStream(libvirt.VIR_STREAM_NONBLOCK)
         #domain.openConsole(None,stream, 0)
         try:
-            res = domain.create()   #virDomainRestore /virDomainResume
-        except libvirtError as e:
-            raise RuntimeError("VM is already running")
-    #domain.reboot() 
+            state = self.getDomStatus(dom)
+            #dom.create()   #virDomainRestore /virDomainResume
+            if state == 3:
+                dom.resume()   #virDomainRestore /virDomainResume
+                return "VM sucessfully resumed"
+            elif state == 5:
+                dom.create()
+                return "VM sucessfully restarted"
+            elif state == 1:
+                raise DomainAlreadyRunning()
+
+        except libvirtError as e: #Except: Domain is already running
+            return e                
+        #domain.reboot() 
     
     def stopVM(self, id):
-        domain = self.getDomainByUUID(id)
-    
-        return domain.suspend() #suspend- speichert das persistente Image nicht 
-        #.managedSave()
+        conn = self.libvirtConnect()
+        dom = self.getDomainByUUID(id)
+        try:
+            state = self.getDomStatus(dom)
+            if state == 1:
+                dom.suspend() #suspend- speichert das persistente Image nicht 
+                return "VM sucessfully stopped"
+            else:
+                raise DomainNotRunning()
+        except libvirtError as e:
+            return e
+            
+    def shutdownVM(self, id, save):
+        conn = self.libvirtConnect()
+        dom = self.getDomainByUUID(id)
+        try:
+            if save:
+                dom.managedSave()
+                return "VM sucessfully saved"
+            else:
+                dom.destroy() #Erzwingt das Herunterfahren
+            return "VM sucessfully shutdown"
+        except libvirtError as e:
+            if e.get_error_code() == 55:
+                raise DomainNotRunning()
 
-    def shutdownVM(self, id):
-        domain = self.getDomainByUUID(id)
-        return domain.destroy() #Erzwingt das Herunterfahren
+    def deleteVM(self, id):
+        conn = self.libvirtConnect()
+        dom = self.getDomainByUUID(id)
+        try:
+            dom.undefine()
+            #Aktive Vm wird lediglich pausiert
+            if dom.isActive == False:
+                return "VM was paused, not deleted" 
+            return "VM was sucessfully deleted"
+        except libvirtError as e:
+            raise APIError(str(e)) ## Error: Cannot undefine transient domain -> Wenn VM am laufen ist?
+        
+    def runVM_xml(self, body):
+            conn = self.libvirtConnect()
+            xmlconfig = xml.dom.minidom.parseString(body)
+            new_xml = xmlconfig.toprettyxml()
+            dom = None
+            try:
+                dom = self.conn.defineXMLFlags(new_xml, 0)
+                dom.create()
+                return {"Following guest sucessfully booted": {"Id" : dom.UUIDString(), "Name": dom.name()},
+                        "info": "For further parameters visit: https://libvirt.org/formatdomain.html"}
+            except libvirtError as e:
+                raise APIError(str(e))
 
-    def runVM(self, obj: BaseModel):
+    def runVM_json(self, obj: BaseModel):
+        conn = self.libvirtConnect()
         dict = obj.dict() 
-        xmlconfig = f'''<domain type='kvm'>
+        xmlconfig = f'''
+          <domain type='kvm'>
         ​  <name>{dict.get("name")}</name>
         <!--
         ​  <uuid>c7a5fdbd-cdaf-9455-926a-d65c16db1809</uuid>
@@ -95,14 +182,14 @@ class VM():
         ​  <on_reboot>restart</on_reboot>
         ​  <on_crash>destroy</on_crash>
         ​  <devices>
-        ​    <emulator>/usr/bin/qemu-system-x86_64</emulator>
+        ​    <emulator>{dict.get("emulator")}</emulator>
         ​    <disk type='file' device='disk'>
         ​      <source file='{dict.get("source_file")}'/>
         ​      <driver name='qemu' type='raw'/>
         ​      <target dev='hda'/>
         ​    </disk>
         <!--
-        ​    <interface type='bridge'>
+           <interface type='bridge'>
         ​      <mac address='52:54:00:d8:65:c9'/>
         ​      <source bridge='br0'/>
         ​    </interface>
@@ -111,23 +198,44 @@ class VM():
         ​    <graphics type='vnc' port='-1' listen='127.0.0.1'/>
         ​  </devices>
         ​</domain>'''
-
+        
         dom = None
         try:
-            dom = self.conn.defineXMLFlags(xmlconfig, 0)
-        except libvirt.libvirtError as e:
-            return e
-        if dom.create() < 0:
-            return "Can not boot guest domain"
+            dom = conn.defineXMLFlags(xmlconfig, 0)
+            dom.create()
+            return {"Following guest sucessfully booted": {"Id" : dom.UUIDString(), "Name": dom.name()},
+                    "info": "For further parameters visit: https://libvirt.org/formatdomain.html"}
+            #dom.openGraphics()
+            #dom.openConsole()
+        except libvirtError as e:
+            raise APIError(str(e))
+        
+    def getXMLConfig(self, part: str | None = None):
+        xmlConfig = ""
+        return xmlConfig
+    
+    def getHardwareSpecs(self, part: str | None = None):
+        if part:
+            hardwareSpecs = part
+        else: 
+            hardwareSpecs = "Hello World"
+        return hardwareSpecs
+    
+    def getDomStatus(self, dom):
+        state, maxmem, mem, cpus, cput = dom.info()
+        str = ""
+        if state == 1:
+            str = "Running"
+        elif state == 3:
+            str = "Stopped(Paused)"
+        elif state == 5:
+            str = "Not Running"
+        return {"state": state,
+                "desc": str
+                }#1 = Running, 3 = paused, 5 = saved, shutoff
 
-        return "Guest "+ dom.name() + "has booted"
 
-    def getDomainByUUID(self, id):
-        domains = self.conn.listAllDomains()
-        for dom in domains:
-            uuid = dom.UUIDString()
-            if uuid == id:
-                return dom
+
 
 
 
