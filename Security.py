d9c799cd86353118d19379e43937ce87c3067d5a
DanB1999 <83217388+DanB1999@users.noreply.github.com>
2023-05-31 19:55:03 +0200

    Update Security.py

--- Security.py
+++ Security.py
@@ -56,7 +56,7 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 oauth2_scheme = OAuth2PasswordBearer(
     tokenUrl="token",
-    scopes={"user": "Basic read permissions on ressources", "admin": "Write permissions on ressources"}
+    scopes={"basic": "Basic ead permission on ressources", "advanced": "Write and run permission on ressources"}
 )
 
 def verify_password(plain_password, hashed_password):
@@ -90,10 +90,10 @@ def create_access_token(data: dict, expires_delta: timedelta | None = None):
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
 
-async def get_current_user(
+async def get_current_active_user(
         security_scopes: SecurityScopes, token: Annotated[str, Depends(oauth2_scheme)]
 ):
-    #
+    #Setzt alle benötigten Scopes in der Header des Response
     if security_scopes.scopes:
         authenticate_value = f'Bearer scope="{security_scopes.scope_str}"'
     else:
@@ -103,6 +103,7 @@ async def get_current_user(
         detail="Could not validate credentials",
         headers={"WWW-Authenticate": authenticate_value},
     )
+    #Dekodiert das JWT-Token und liest username und Scopes heraus, formale Validierung durch Pydantic-Model
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         username: str = payload.get("sub")
@@ -112,9 +113,13 @@ async def get_current_user(
         token_data = TokenData(scopes=token_scopes, username=username)
     except (JWTError, ValidationError):
         raise credentials_exception
+    
+    #Überprüft ob User in der "Datenbank" vorhanden
     user = get_user(fake_users_db, username=token_data.username)
     if user is None:
         raise credentials_exception
+    
+    #Prüft, ob er alle abhängigen Scopes vorhanden sind
     for scope in security_scopes.scopes:
         if scope not in token_data.scopes:
             raise HTTPException(
@@ -122,11 +127,7 @@ async def get_current_user(
                 detail="Not enough permissions",
                 headers={"WWW-Authenticate": authenticate_value},
             )
-    return user
-
-async def get_current_active_user(
-    current_user: Annotated[User, Depends(get_current_user)]
-):
-    if current_user.disabled:
+    #Prüft, ob der Benutzer inaktiv ist
+    if user.disabled:
         raise HTTPException(status_code=400, detail="Inactive user")
-    return current_user
+    return user
